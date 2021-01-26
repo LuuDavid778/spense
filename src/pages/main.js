ad958f7a126f531f8fbb628f52b432cbf0a06ef4
luudavid778 <luudavid778@gmail.com>
2021-01-26 14:42:22 -0800

    Installed storybook, setup main page

---
+++ src/pages/main.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const Main = () => {
+  return <div className="main">
+
+</div>
+}
+
+
+Main.defaultProps={
+}
+
+
+export default Main;

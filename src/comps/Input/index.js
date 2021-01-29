dbfe5ade7a1d20a57f717c4d0530f58eb8aae9b8
=Yunus Emre <=yeagacdan@gmail.com>
2021-01-28 20:37:15 -0800

    Components Complated

---
+++ src/comps/Input/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+
+const InputLabel = styled.label`
+font-family: Roboto;
+font-size: 14px;
+color: #676767;
+`;
+
+const InputCont = styled.input`
+min-height: 42px;
+min-width: 350px;
+margin-top: 4px;
+border-radius: 5px;
+::placeholder{
+    font-family: Roboto;
+    font-size: 14px;
+    color: #C4C4C4};
+border:solid 1px #DDD9D9;
+`;
+
+const Input = ({placeholder, text}) => {
+  return <>
+  <InputLabel>{text}</InputLabel>
+  <InputCont placeholder={placeholder}/>
+  </>
+}
+
+Input.defaultProps = {
+placeholder: 'Transaction Name',
+text: 'Transaction Name'
+}
+
+export default Input;

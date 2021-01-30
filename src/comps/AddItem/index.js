f402b3def9e786527888304944c74b55f53c6837
JoshPaulson1 <55604836+JoshPaulson1@users.noreply.github.com>
2021-01-30 12:08:10 -0800

    changes to additem

--- src/comps/AddItem/index.js
+++ src/comps/AddItem/index.js
@@ -3,26 +3,24 @@ import styled from 'styled-components'
 
 const Container = styled.div`
 min-height: 66px;
-max-width: ${props => props.width ? props.width : "66px"};
+max-width: 66px;
 min-width: 10px;
 border-radius: 100px;
-background-color: ${props => props.bgcolour ? props.bgcolour : "#698FF2"};
+background-color:#698FF2;
 display:flex;
 align-items:center;
 justify-content:center;
 `;
 
-const AddItem = ({label, bwidth, bcolour}) => {
+const AddItem = ({label}) => {
 
-    return <Container bgcolour={bcolour} width={bwidth}>
+    return <Container>
     {label}
     </Container>
 }
 
 AddItem.defaultProps = {
-label: "+",
-bwidth: null,
-bcolour: null
+label: "+"
 }
 
 export default AddItem

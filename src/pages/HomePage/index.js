43753162d7f41bd3c426153f1c5b5a53415bf000
=Yunus Emre <=yeagacdan@gmail.com>
2021-01-30 22:52:49 -0800

    page start

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import TotalAmount from '../../comps/TotalAmount';
+import DropDown from '../../comps/DropDown';
+import Selection from '../../comps/Selection';
+import Date from '../../comps/Date';
+import Transaction from '../../comps/Transaction';
+import AddItem from '../../comps/AddItem';
+
+
+export default function HomePage(){
+    return(
+        <div>
+
+        </div>
+    )}

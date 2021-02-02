9ecba75274aa22a61d89bf2c7972d0aee1bef0f1
luudavid778 <luudavid778@gmail.com>
2021-02-01 21:18:22 -0800

    Reinstalled SASS

--- src/pages/opentrans/index.js
+++ src/pages/opentrans/index.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+import Button from 'comps/Button';  
 import './opentrans.scss'
 
 
 const OpenTrans = () => {
+
+
   return <div className="main">
 
 </div>
